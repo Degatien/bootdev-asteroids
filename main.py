d55d115c9bdfa8a8435326bb364cb184f4e177cb
Degatien <degatien@gmail.com>
2024-08-22 09:56:24 +0000

    [UPDATE] Use of containers
    Containers allows to update a whole groupe of sprite and draw them
    without manually calling for them

--- main.py
+++ main.py
@@ -15,14 +15,21 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        for sprite in updatable:
+            sprite.update(dt)
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        for sprite in drawable:
+            sprite.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 60
         
