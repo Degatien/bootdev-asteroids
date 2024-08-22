8d714e849383bed583033dfbbb314baa3ec02104
Degatien <degatien@gmail.com>
2024-08-22 10:07:45 +0000

    [ADD] Add asteroids and asteroid field
    The AsteroidField class is used to generate random asteroids
    The Asteroid class represent an asteroid that move accross the screen

--- main.py
+++ main.py
@@ -4,6 +4,8 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     print("Starting asteroids!")
@@ -15,11 +17,15 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     
+    asteroids = pygame.sprite.Group()
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
 
+    asteroid_field = AsteroidField()
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     while True:
         for event in pygame.event.get():
