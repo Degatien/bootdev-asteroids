e166684dc21f906eecdf3f48ba41af6069c5be2b
Degatien <degatien@gmail.com>
2024-08-23 11:48:52 +0000

    [ADD] Add the ability to shoot bullet
    Press spacebar to send bullet from your player

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
-
+from shot import Shot
 def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -19,12 +19,14 @@ def main():
     dt = 0
     
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable)
+    Shot.containers = (updatable, drawable, shots)
 
     asteroid_field = AsteroidField()
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
